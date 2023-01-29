d99ca65ae9ad7030d83bd43b92bc49e35ee38870
Daniel Baird <danielbaird@Daniels-MacBook-Pro.local>
2023-01-28 22:35:48 -0800

    other

--- game.py
+++ game.py
@@ -1,23 +1,23 @@
 # https://pygame-gui.readthedocs.io/en/latest/quick_start.html
- import pygame
+import pygame
 
 
- pygame.init()
+pygame.init()
 
- pygame.display.set_caption('Quick Start')
- window_surface = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Quick Start')
+window_surface = pygame.display.set_mode((800, 600))
 
- background = pygame.Surface((800, 600))
- background.fill(pygame.Color('#000000'))
+background = pygame.Surface((800, 600))
+background.fill(pygame.Color('#000000'))
 
- is_running = True
+is_running = True
 
- while is_running:
+while is_running:
 
-     for event in pygame.event.get():
-         if event.type == pygame.QUIT:
-             is_running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
 
-     window_surface.blit(background, (0, 0))
+    window_surface.blit(background, (0, 0))
 
-     pygame.display.update()
+    pygame.display.update()
